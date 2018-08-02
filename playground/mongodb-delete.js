3d0d08fb95db28f47c661a28973d2dc79ad43824
yash <mehtayash001@gmail.com>
2018-08-02 22:41:57 +0530

    Add update script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,23 @@
+ const {MongoClient,ObjectID} = require('mongodb');
+ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db) => {
+if (err) {
+	return console.log('Unable to connect to MongoDB server');
+}	
+	console.log('connected to MongoDB server');
+ 	
+ /*	db.collection('Users').deleteMany({location:'virar'}).then((result) => {
+ 		console.log(result);
+ 	});
+ 	console.log('deleted');
+ 	*/
+	//db.close();
+
+ /*	db.collection('Users').deleteOne({location:'new york'}).then((result) => {
+ 		console.log(result);
+ 	});*/
+
+ 	db.collection('Users').findOneAndDelete({name:'Something to do1'}).then((result) => {
+ 		console.log(result);
+ 	}); 
+});
+  
---
+++ playground/mongodb-update.js
@@ -0,0 +1,35 @@
+ const {MongoClient,ObjectID} = require('mongodb');
+ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db) => {
+if (err) {
+	return console.log('Unable to connect to MongoDB server');
+}	
+	console.log('connected to MongoDB server');
+ 	/*
+	 db.collection('Todos').find({_id: new ObjectID('5b61c8b82086bc7812404a44')}).toArray().then((docs) => {
+		console.log(JSON.stringify(docs,undefined,2))
+	},(err) => {
+		console.log('Unable to fetch todos');
+	});*/
+
+ 	db.collection('Todos').findOneAndUpdate({
+ 	_id: new ObjectID('5b61c8b82086bc7812404a44')
+ 	}, {
+ 		$set: {
+ 			text: 'yash m'
+ 		},
+ 		$inc: {
+ 				age: 5
+ 		 }
+ 	   },
+ 	   
+ 			
+ 	    {
+ 	    	returnOriginal: false
+ 	}).then((result) => {
+
+ 		console.log(result);
+ 		console.log("completed");
+
+ 	}); 
+});
+  
