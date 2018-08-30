443632ed60fb83394fdaddac2a9fb3e92f0c043f
yash <mehtayash001@gmail.com>
2018-08-30 20:33:12 +0530

    Test the DELETe /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -107,3 +107,47 @@ it('should return 404 for non-object ids',(done) => {
 });
 
 });
+
+describe('DELETE / todos/:id',() => {
+it('should remove a todo',(done)=> {
+  var hexId = todos[1]._id.toHexString();
+  request(app)
+  .delete(`/todos/${hexId}`)
+  .expect(200)
+  .expect((res) => {
+    expect(res.body._id).toBe(hexId);
+  })
+  .end((err,res) => {
+    if(err) {
+      return done(err);
+      }
+    });
+   
+  //query database using findById toNotExist
+  Todo.findById(hexId).then((todo) => {
+  expect(todo).toBeTruthy();
+  done();
+  }).catch((e) => done(e)); 
+
+ });
+  
+  it('should return 404 if todo not found',(done) => {
+ var hexId = new ObjectID().toHexString();
+ request(app) 
+ .delete(`/todos/${hexId}`)
+ .expect(404)
+ .end(done);  
+   
+});
+
+
+it('should return 404 for non-object ids',(done) => {
+//
+   request(app)
+   .delete('/todos/123abc')
+   .expect(404)
+   .end(done);
+});
+
+
+});
